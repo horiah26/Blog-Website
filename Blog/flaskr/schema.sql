55046cc6cdfe4398deae4549b5d8c9cfabaf4c0c
horiah26 <57143792+horiah26@users.noreply.github.com>
2021-07-06 16:16:26 +0300

    Done all without tests

---
+++ Blog/flaskr/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS user_tab;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user_tab(
+  id INT PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE post (
+  id INTEGER PRIMARY KEY,
+  author_id INTEGER NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user_tab (id)
+);
