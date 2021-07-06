a41d9db3627b8e8b66c8b72848d47424cca7d111
horiah26 <57143792+horiah26@users.noreply.github.com>
2021-07-06 08:42:20 +0300

    Shows snake

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
 app = Flask(__name__)
 
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
+@app.route("/")
+def index():
+    return "Index"
 
-@app.route('/')
-@app.route('/hello')
-def hello():
-    # Render the page
-    return "Hello Python!"
+@app.route("/hello/<name>")
+def hello(name):
+    return render_template(
+    'test.html',name=name)
 
-if __name__ == '__main__':
-    # Run the app server on localhost:4449
-    app.run('localhost', 4449)
+@app.route("/members")
+def members():
+    return "Members"
 
-@app.route('/')
-@app.route('/hello')
-def hello():
-    # Render the page
-    return "Hello Python!"
+@app.route("/members/<string:username>")
+def getMember(username):
+    return username
 
-if __name__ == '__main__':
-    # Run the app server on localhost:4449
-    app.run('localhost', 4449)
+if __name__ == "__main__":
+    app.run()
