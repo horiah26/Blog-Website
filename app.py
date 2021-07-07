a093afdb7b83632b82f78c8622770f79d65dc327
horiah26 <57143792+horiah26@users.noreply.github.com>
2021-07-07 16:02:30 +0300

    shows all on index

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from models.post import Post
 from repositories.list import post_list
 
 def create_app():
@@ -7,13 +6,7 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY="secret")
     
-    post1 = Post(1, "title", "text 1")
-    post2 = Post(2, "title2", "text 2")
-    post3 = Post(2, "title 3", "text 3")
-    post4 = Post(2, "title 3", "text 3")
 
-    post_list.append(post1)
-    post_list.append(post2)
 
     import blog
     app.register_blueprint(blog.bp)
--- repositories/list.py
+++ repositories/list.py
@@ -1,1 +1,14 @@
+
+from models.post import Post
+
 post_list =[]
+
+post1 = Post(1, "title 1", "text 1")
+post2 = Post(2, "title 2", "text 2")
+post3 = Post(3, "title 3", "text 3")
+post4 = Post(4, "title 4", "text 4")
+
+post_list.append(post1)
+post_list.append(post2)
+post_list.append(post3)
+post_list.append(post4)
